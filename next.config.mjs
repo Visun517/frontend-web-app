0916b2434c8e16e80ef1a2fa737e1368a587ea05
Visun <visunpraboda999@gmail.com>
2026-08-26 01:31:36 +0530

    Update application configuration for cloud and add ecosystem config for services

--- next.config.mjs
+++ next.config.mjs
@@ -5,20 +5,14 @@ const nextConfig = {
       {
         protocol: "http",
         hostname: "localhost",
-        port: "7000",
-        pathname: "/api/v1/students/**",
       },
       {
         protocol: "http",
         hostname: "34.36.169.22",
-        port: "",
-        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "storage.googleapis.com",
-        port: "",
-        pathname: "/**",
       }
     ],
   },
